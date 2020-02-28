76ac894e8f7555fd5ca76296d2b269563a2621f6
Erik Hanchett <erik.hanchett@gmail.com>
2020-02-28 14:30:42 -0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/TestGithubPages/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/TestGithubPages" : "/",
   outputDir: "docs"
 };
